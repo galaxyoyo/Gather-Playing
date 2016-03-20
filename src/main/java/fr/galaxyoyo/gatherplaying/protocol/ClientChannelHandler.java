60ecc2a54844dd527cd75ea455a714c6cc14fbe5
galaxyoyo <yohann.danello@gmail.com>
2016-03-20 22:12:36 +0100

    Connection lost warning message

--- src/main/java/fr/galaxyoyo/gatherplaying/protocol/ClientChannelHandler.java
+++ src/main/java/fr/galaxyoyo/gatherplaying/protocol/ClientChannelHandler.java
@@ -1,23 +1,21 @@
 package fr.galaxyoyo.gatherplaying.protocol;
 
 import fr.galaxyoyo.gatherplaying.Player;
+import fr.galaxyoyo.gatherplaying.Utils;
 import fr.galaxyoyo.gatherplaying.client.Client;
 import fr.galaxyoyo.gatherplaying.client.gui.LoginDialog;
 import io.netty.channel.ChannelHandlerAdapter;
 import io.netty.channel.ChannelHandlerContext;
 import javafx.application.Platform;
-
-import java.io.IOException;
+import javafx.scene.control.Alert;
 
 public class ClientChannelHandler extends ChannelHandlerAdapter
 {
 	@Override
 	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception
 	{
-		if (cause instanceof IOException)
-			System.err.println(cause.getClass().getName() + " : " + cause.getMessage());
-		else
-			cause.printStackTrace();
+		Platform.runLater(() -> Utils.alert("Erreur de connexion", "Problème de connexion avec le serveur", "Un problème de connexion est survenu. Malheureusement, il n'y a pas " +
+				"encore de système de reconnexion de géré, le jeu va désormais se fermer.", Alert.AlertType.WARNING).ifPresent(buttonType -> System.exit(-1)));
 	}
 
 	@Override
